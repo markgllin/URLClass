b3477036c0bc239e9cfe371dcfa5e829dc0a5782
Mark <markgllin@gmail.com>
2016-10-10 14:36:44 -0600

    last-modded date not added yet

--- UrlCache.java
+++ UrlCache.java
@@ -9,6 +9,8 @@ package urlrequest;
 import java.util.HashMap;
 import java.io.*;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 public class UrlCache {
 
@@ -32,20 +34,31 @@ public class UrlCache {
      */
 	public UrlCache() throws UrlCacheException {
 
-/*		File cacheDir = new File("cache");
+		File cacheDir = new File("cache");
+        File catalogFile = new File("cache/catalog");
 
         //create cache directory
         if(!cacheDir.isDirectory()) cacheDir.mkdir();
 
+        //create/read catalog file
         try{
-            PrintWriter catalog = 
-                new BufferedWriter(
-                    new FileWriter("cache/catalog", true));
+            catalogFile.createNewFile();
+
+            BufferedReader br = new BufferedReader(new FileReader("cache/catalog"));
+            String line;
+            
+            catalog = new HashMap<String, String>();
+
+            while((line = br.readLine()) != null){
+                String[] parts = line.split("_");
+                catalog.put(parts[0], parts[1]);
+            }
+
         }catch(IOException e){
             e.printStackTrace();
         }
-*/
-        catalog = new HashMap();
+
+        
 	}
 	
     /**
@@ -71,18 +84,36 @@ public class UrlCache {
 
             //send http request to server
             out.println(parsedUrl[REQ]);
-            
-            //read response from server
-            while((output = in.readLine()) != null)
-                System.out.println(output);
+
+            //create directory structuce for objects to be downloaded
+            File urlObject = new File("cache/"+url);
+            File parent = urlObject.getParentFile();
+            if(!parent.exists() && !parent.mkdirs()){
+                throw new IllegalStateException("Couldn't create dir: " + parent);
+            }
+
+            urlObject.createNewFile();
+
+            PrintWriter urlOutStream = new PrintWriter(
+                    new BufferedWriter(
+                        new FileWriter("cache/"+url, true)));
+ 
+            //write response from server to file
+            while((output = in.readLine()) != null){
+                urlOutStream.println(output);
+            }
+                
         
+            urlOutStream.close();
+            catalog.put(url, parseLastModified(url)); // change to date
+
         }catch(NumberFormatException | IOException e){
             e.printStackTrace();
         }
 	}
 
     public String[] parseUrl(String url){
-        String host, filepath;
+        String host, filepath, fullRequest, lastModified;
         String port = "80";
 
         //parse for filepath
@@ -98,10 +129,18 @@ public class UrlCache {
             host = parts[0].trim();
         }
 
+        //get last modified date
+        try{
+            lastModified = catalog.get(url); 
+        }catch (UrlCacheException | NullPointerException e){
+            lastModified = "Thu, 01 Jan 1970 00:00:00 UTC";
+        }
+
         //build http request
-        String fullRequest = "GET " + filepath + " HTTP/1.1\r\n"
-                            + "Host: " + host + ":" + port + "\r\n"
-                            + "\r\n";
+        fullRequest = "GET " + filepath + " HTTP/1.1\r\n"
+                        + "Host: " + host + ":" + port + "\r\n"
+                        + "If-Modified-Since: " + lastModified + "\r\n"
+                        + "\r\n";
         
         return new String[] {host, port, fullRequest};
     }
@@ -114,7 +153,29 @@ public class UrlCache {
      * @throws UrlCacheException if the specified url is not in the cache, or there are other errors/exceptions
      */
 	public long getLastModified(String url) throws UrlCacheException {
-		return -1;
+        try{
+            return new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss zzz").parse(catalog.get(url)).getTime();
+        }catch(ParseException e){
+            throw new UrlCacheException();
+        }
 	}
+
+    public String parseLastModified(String url){
+        String line;
+
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("cache/" + url));
+            while((line = br.readLine()) != null){
+                if(line.contains("Last-Modified:")){
+                    String[] parts = line.split(":", 2);
+                    return parts[1].trim();
+                }
+            }
+        }catch(IOException e){
+            System.out.println("incorrect path");
+        }
+
+        return null;
+    }
 
 }
